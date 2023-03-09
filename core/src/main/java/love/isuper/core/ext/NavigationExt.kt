af7df79a9d7d42fce49d4f69982c0b5d236b88c9
guoshichao <guoshichao@glazero.com>
2023-03-09 19:58:08 +0800

    [描述]:优化添加跳转动画 [模块]:

--- core/src/main/java/love/isuper/core/ext/NavigationExt.kt
+++ core/src/main/java/love/isuper/core/ext/NavigationExt.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import love.isuper.core.R
 
 /**
  * Created by shichao on 2022/5/18.
@@ -20,15 +22,33 @@ fun nav(view: View): NavController {
     return Navigation.findNavController(view)
 }
 
-var lastNavTime = 0L
 
+private val defaultNavOptions = NavOptions.Builder()
+    .setEnterAnim(R.anim.page_enter_anim)
+    .setExitAnim(R.anim.page_exit_anim)
+    .setPopEnterAnim(R.anim.page_pop_enter_anim)
+    .setPopExitAnim(R.anim.page_pop_exit_anim)
+    .setLaunchSingleTop(true)
+    .build()
+/**
+ * 跳转Fragment
+ * @param resId 跳转的action Id
+ * @param bundle 传递的参数
+ * @param navOptions 动画
+ */
+fun NavController.navigateAction(resId: Int, bundle: Bundle? = null, navOptions: NavOptions = defaultNavOptions) {
+    navigate(resId, bundle, navOptions)
+}
+
+
+var lastNavTime = 0L
 /**
  * 防止短时间内多次快速跳转Fragment出现的bug
  * @param resId 跳转的action Id
  * @param bundle 传递的参数
  * @param interval 多少毫秒内不可重复点击 默认0.5秒
  */
-fun NavController.navigateAction(resId: Int, bundle: Bundle? = null, interval: Long = 500) {
+fun NavController.navigateSingleAction(resId: Int, bundle: Bundle? = null, interval: Long = 500) {
     val currentTime = System.currentTimeMillis()
     if (currentTime >= lastNavTime + interval) {
         lastNavTime = currentTime
